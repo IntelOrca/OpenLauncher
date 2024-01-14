5b14e1bb7143df6324e7186c597dc38d670f1d90
samuel-w <samuel.woon@protonmail.com>
2024-01-14 13:15:28 -0600

    Fix test

--- test/IntelOrca.OpenLauncher.Tests/BuildServiceTests.cs
+++ test/IntelOrca.OpenLauncher.Tests/BuildServiceTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using IntelOrca.OpenLauncher.Core;
 using Xunit;
@@ -13,13 +14,14 @@ namespace IntelOrca.OpenLauncher.Tests
         {
             var buildService = new BuildService();
             var builds = await buildService.GetBuildsAsync(Game.OpenLoco, includeDevelop: false);
-            var build = builds.First(x => x.Version == "v22.05.1");
+            var build = builds.First(b => b.Version == "v22.05.1");
+            var buildAsset  = build.Assets.First(ba => ba.Platform == OSPlatform.OSX);
             Assert.Equal("v22.05.1", build.Version);
             Assert.Equal(new DateTime(2022, 5, 17, 20, 6, 15), build.PublishedAt);
-            Assert.Equal("OpenLoco-v22.05.1-macos.zip", build.Assets[0].Name);
-            Assert.Equal(new Uri("https://github.com/OpenLoco/OpenLoco/releases/download/v22.05.1/OpenLoco-v22.05.1-macos.zip"), build.Assets[0].Uri);
-            Assert.Equal("application/x-zip-compressed", build.Assets[0].ContentType);
-            Assert.Equal(4157592, build.Assets[0].Size);
+            Assert.Equal("OpenLoco-v22.05.1-macos.zip", buildAsset.Name);
+            Assert.Equal(new Uri("https://github.com/OpenLoco/OpenLoco/releases/download/v22.05.1/OpenLoco-v22.05.1-macos.zip"), buildAsset.Uri);
+            Assert.Equal("application/x-zip-compressed", buildAsset.ContentType);
+            Assert.Equal(4157592, buildAsset.Size);
         }
     }
 }
